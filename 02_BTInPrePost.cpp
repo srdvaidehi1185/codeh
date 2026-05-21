1985031a25ce3367140f04ee37da4b4c979e127f
srdvaidehi1185 <srdvaidehi1185@gmail.com>
2026-05-21 07:17:21 +0530

    Update 02_BTInPrePost.cpp

--- 02_BTInPrePost.cpp
+++ 02_BTInPrePost.cpp
@@ -15,14 +15,35 @@ Node* newNode(int val) {
     return temp;
 }
 
-Node* insert(Node* root, int val) {
+Node* insert(Node* root , int val){
+    Node* newnode = newNode(val);
     if(root == NULL){
-        return newNode(val);
+        return newnode;
     }
-    if(val < root->data){
-        root->left = insert(root->left, val);
+
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp->left == NULL){
+            temp->left = newnode;
+            return root;
+        }
+        else{
+            q.push(temp -> left);
+        }
+
+        if(temp->right == NULL){
+            temp->right = newnode;
+            return root;
+        }
+        else{
+            q.push(temp -> right);
+        }
     }
-    root->right = insert(root->right, val);
 
     return root;
 }
